8e1dd76a4371d56a62aea623c7777429261af1b0
Jesper Elmtoft <113169402+JesperE98@users.noreply.github.com>
2023-11-17 13:59:30 +0100

    Light, Audio and SceneManager Update on Hotel Room
    Updated the light settings in Hotel Room Scene so you get a more cozy feeling.
    
    Added Urban City Ambience soundtrack to the Scene
    
    Fixed the SceneTransitionManagerScript so it goses to next scene.

--- Avicii The Last Concert/Assets/Jesper/Scripts/AudioManager.cs
+++ Avicii The Last Concert/Assets/Jesper/Scripts/AudioManager.cs
@@ -2,16 +2,29 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    
+    private static AudioManager _instance;
+    public static AudioManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("AudioManager is null!!");
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+
     [SerializeField]
     protected AudioSource[] m_AudioSources;
     [SerializeField]
     protected int m_AudioTrackNumber;
-
-    private void Awake()
-    {
-        
-    }
 
     public void PlayAudioTrack(int audioTrackNumber)
     {       
